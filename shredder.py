24211cf6c04e9951c462c75c4bdb09de4a5616da
ehopealot <erikjhope@gmail.com>
2012-08-26 15:57:47 -0500

    Adds shredder and sample image

---
+++ shredder.py
@@ -0,0 +1,31 @@
+from PIL import Image
+from random import shuffle
+import argparse
+import os.path
+
+
+parser = argparse.ArgumentParser(description='Shred an image')
+parser.add_argument('--shreds', default=20, type=int)
+parser.add_argument('--filename', default='sample.png')
+args = parser.parse_args()
+
+SHREDS = args.shreds
+filename = args.filename
+
+
+image = Image.open(filename)
+shredded = Image.new('RGBA', image.size)
+width, height = image.size
+shred_width = width/SHREDS
+sequence = range(0, SHREDS)
+shuffle(sequence)
+
+for i, shred_index in enumerate(sequence):
+    shred_x1, shred_y1 = shred_width * shred_index, 0
+    shred_x2, shred_y2 = shred_x1 + shred_width, height
+    region =image.crop((shred_x1, shred_y1, shred_x2, shred_y2))
+    shredded.paste(region, (shred_width * i, 0))
+
+split_filename = os.path.splitext(filename)
+print split_filename
+shredded.save('{0}_shredded{1}'.format(*split_filename))
